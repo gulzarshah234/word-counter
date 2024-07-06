0234a91302cfb0b97c5455d8bd08b5cd367f2425
gulzarshah234 <gulzarshah4326417@gmail.com>
2024-07-06 04:21:18 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+
+const answers: {
+    Sentence: string}= await inquirer.prompt([
+        {
+            name: "Sentence",
+            type: "input",
+            message: "Enter Your Sentence to count the word:"
+        },
+    ])
+
+    const words = answers.Sentence.trim().split(" ")
+    console.log(words);
+    console.log(`Your Sentence word count is ${words.length}`)     
+    
